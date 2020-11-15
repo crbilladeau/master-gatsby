2cd4cd9e49a594290a6b9ca34430914aa9c8dfd4
Charlie Billadeau <charlie.billadeau@gmail.com>
2020-11-15 15:28:52 -0600

    updating for responsiveness

--- gatsby/src/styles/Grids.js
+++ gatsby/src/styles/Grids.js
@@ -3,7 +3,11 @@ import styled from 'styled-components';
 export const HomePageGrid = styled.div`
   display: grid;
   gap: 2rem;
-  grid-template-columns: repeat(2, minmax(auto, 1fr));
+  --columns: 2;
+  grid-template-columns: repeat(var(--columns), minmax(auto, 1fr));
+  @media (max-width: 800px) {
+    --columns: 1;
+  }
 `;
 
 export const ItemsGrid = styled.div`
@@ -21,10 +25,13 @@ export const ItemStyles = styled.div`
     font-size: 0;
   }
   p {
+    top: 0;
     transform: rotate(-2deg) translateY(-140%);
     position: absolute;
     width: 100%;
     left: 0;
+    margin: 0;
+    font-size: clamp(12px, 5vw, 20px);
   }
   .mark {
     display: inline;
